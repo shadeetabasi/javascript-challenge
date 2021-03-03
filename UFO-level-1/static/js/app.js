6ab00a6662ce4446757aea57965a398ec6bf6c58
Shadee Tabasi <shadeetabasi@gmail.com>
2021-03-02 19:03:16 -0500

    updated js file

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -29,16 +29,39 @@ function runEnter() {
     var inputValue = inputDate.property("value");
 
     console.log(inputValue);
-    console.log(tableData);
-
+    
+    // Filter date by date and console the return values
     var filteredData = tableData.filter(data => data.datetime === inputValue);
 
     console.log(filteredData);
-  
+};
 
+tableData.forEach(function(alienSighting) {
+    console.log(alienSighting);
+    var row = tbody.append("tr");
 
+    Object.defineProperties(alienSighting). forEach(function([key, value]) {
+        console.log(key, value);
 
+        var cell= row.append("td");
+        cell.text(value)
 
+    });
 
+   });
 
-}
+// // // // Set the tbody element to the text (for now and then run a table)
+// //     // d3.select("tbody").text
+
+// //     // Step 2:  Use d3 to append one table row `tr` for each weather report object
+// //     // Don't worry about adding cells or text yet, just try appending the `tr` elements.
+// // data.forEach(function(aliensSightings) {
+// //     console.log(alienSightings);
+// //     var row = tbody.append("tr");
+// // };
+
+// //     Object.defineProperties(aliensSightings).forEach(function([key, value]) {
+// //         console.log(key, value);
+        
+// }
+
