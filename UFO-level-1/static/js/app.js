4d678824cd4dc48e024737c8cc0ba60365bc48ba
Shadee Tabasi <shadeetabasi@gmail.com>
2021-03-04 18:26:06 -0500

    completed first cahellenge

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,13 +1,8 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-
 // Select the button
 var button = d3.select("#filter-btn")
-
-// // Select the datetime input
-// var inputDate = d3.select("#datetime")
 
 // select body where table data should appear
 var tableBody = d3.select("tbody")
@@ -15,6 +10,20 @@ var tableBody = d3.select("tbody")
 // Create event handlers 
 button.on("click", runEnter);
 tableBody.on("submit",runEnter);
+
+function buildTable(filteredData) {
+    for (var i = 0; i < filteredData.length; i++) {
+        var trow = tableBody.append("tr");
+        var observation = filteredData[i]
+        trow.append("td").text(observation["datetime"]);
+        trow.append("td").text(observation["city"]);
+        trow.append("td").text(observation["state"]);
+        trow.append("td").text(observation["country"]);
+        trow.append("td").text(observation["shape"]);
+        trow.append("td").text(observation["durationMinutes"]);
+        trow.append("td").text(observation["comments"]);
+      }
+};
 
 // Complete the event handler function for the form
 function runEnter() {
@@ -33,35 +42,7 @@ function runEnter() {
     // Filter date by date and console the return values
     var filteredData = tableData.filter(data => data.datetime === inputValue);
 
-    console.log(filteredData);
+    buildTable(filteredData);
+
 };
-
-tableData.forEach(function(alienSighting) {
-    console.log(alienSighting);
-    var row = tbody.append("tr");
-
-    Object.defineProperties(alienSighting). forEach(function([key, value]) {
-        console.log(key, value);
-
-        var cell= row.append("td");
-        cell.text(value)
-
-    });
-
-   });
-
-// // // // Set the tbody element to the text (for now and then run a table)
-// //     // d3.select("tbody").text
-
-// //     // Step 2:  Use d3 to append one table row `tr` for each weather report object
-// //     // Don't worry about adding cells or text yet, just try appending the `tr` elements.
-// // data.forEach(function(aliensSightings) {
-// //     console.log(alienSightings);
-// //     var row = tbody.append("tr");
-// // };
-
-// //     Object.defineProperties(aliensSightings).forEach(function([key, value]) {
-// //         console.log(key, value);
-        
-// }
 
